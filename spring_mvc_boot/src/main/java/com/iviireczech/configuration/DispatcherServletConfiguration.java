e16417b3faf201daafac7dc585a4a5af19e2078e
Miroslav Lacina <miroslav.lacina@gmail.com>
2015-10-28 23:16:22 +0100

    Add boot module

---
+++ spring_mvc_boot/src/main/java/com/iviireczech/configuration/DispatcherServletConfiguration.java
@@ -0,0 +1,22 @@
+package com.iviireczech.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.iviireczech.controller"})
+public class DispatcherServletConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
