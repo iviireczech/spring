52bb12b1681f325a799b7b20805b7618e00da600
Miroslav Lacina <miroslav.lacina@gmail.com>
2015-10-28 23:14:59 +0100

    Add xml module

---
+++ spring_mvc_xml/src/main/java/com/iviireczech/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.iviireczech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getHello() {
+        return "hello";
+    }
+
+}
